785a8a4ccaa858d7d3f3f71127a5b9a8ea59b52a
Answl <118355919+Answl@users.noreply.github.com>
2024-10-04 03:46:25 +0900

    [CHORE] 프로젝트 생성

---
+++ src/main/java/com/smwu_itple/backend/SmwuItpleApplication.java
@@ -0,0 +1,13 @@
+package com.smwu_itple.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmwuItpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmwuItpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smwu_itple/backend/SmwuItpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smwu_itple.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmwuItpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
