8afae8522b584fadaf68abb21c327adbf72cc0c3
Answl <118355919+Answl@users.noreply.github.com>
2025-01-28 23:30:00 +0900

    [refactor] archiveRepository JPA 방식

--- src/main/java/com/smwu_itple/backend/repository/ArchiveRepository.java
+++ src/main/java/com/smwu_itple/backend/repository/ArchiveRepository.java
@@ -4,27 +4,11 @@ import com.smwu_itple.backend.domain.Archive;
 import com.smwu_itple.backend.domain.Late;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class ArchiveRepository {
-    private final EntityManager em;
-
-    public void save(Archive archive) {em.persist(archive);}
-
-    // 모든 아카이브 조회
-    public List<Archive> findAll(){
-        return em.createQuery("select a from Archive a", Archive.class)
-                .getResultList();
-    }
-
-    // 특정 late에 속한 아카이브 조회
-    public List<Archive> findAllByLate(Late late) {
-        return em.createQuery("select a from Archive a where a.late = :late", Archive.class)
-                .setParameter("memorial", late)
-                .getResultList();
-    }
+public interface ArchiveRepository extends JpaRepository<Archive, Long> {
 }
