a1aa49cae947e34d937a40d1691bec93464cf388
Answl <118355919+Answl@users.noreply.github.com>
2025-03-02 01:46:20 +0900

    [del] 불필요한 코드 수정

--- src/main/java/com/smwu_itple/backend/controller/LateController.java
+++ src/main/java/com/smwu_itple/backend/controller/LateController.java
@@ -95,6 +95,7 @@ public class LateController {
         }
     }
 
+    //조문메시지 작성
     @PostMapping(value = "/{lateId}/message", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse> createMessage(@PathVariable Long lateId,
                                                      @RequestPart(value = "attachment", required = false) MultipartFile attachment, // 사진 파일
@@ -102,7 +103,7 @@ public class LateController {
                                                      HttpSession session) {
         try {
             Long senderId = SessionUtil.getUserIdFromSession(session);
-            MessageCreateResponse response = manageService.createMessage(lateId, senderId, attachment, messagecreateRequest);
+            MessageCreateResponse response = lateService.createMessage(lateId, senderId, attachment, messagecreateRequest);
             return ApiResponse.onSuccess(response, SuccessStatus._POST_MESSAGE_CREATE_SUCCESS);
         } catch (UnauthorizedException e) {
             return ApiResponse.onFailure(null, FailureStatus._UNAUTHORIZED, e.getMessage());
@@ -111,13 +112,14 @@ public class LateController {
         }
     }
 
+    //조의금 전송
     @PostMapping("/{lateId}/pay")
     public ResponseEntity<ApiResponse> CreatePay(@PathVariable Long lateId,
                                                  @RequestBody PayCreateRequest paycreateRequest,
                                                  HttpSession session){
         try {
             Long senderId = SessionUtil.getUserIdFromSession(session);
-            PayCreateResponse response = manageService.createPay(lateId, senderId, paycreateRequest);
+            PayCreateResponse response = lateService.createPay(lateId, senderId, paycreateRequest);
             return ApiResponse.onSuccess(response, SuccessStatus._POST_PAY_CREATE_SUCCESS);
         } catch (UnauthorizedException e) {
             return ApiResponse.onFailure(null, FailureStatus._UNAUTHORIZED, e.getMessage());
--- src/main/java/com/smwu_itple/backend/service/ManageService.java
+++ src/main/java/com/smwu_itple/backend/service/ManageService.java
@@ -28,62 +28,6 @@ public class ManageService {
 
     private final LateService lateService;
     private final UserService userService;
-    private final OwnerService ownerService;
-
-    //조문 메시지 전송
-    @Transactional
-    public MessageCreateResponse createMessage(Long lateId, Long senderId, MultipartFile attachment, MessageCreateRequest messageCreateRequest) throws IOException {
-        Late late = lateService.findLateByIdOrThrow(lateId);
-        User sender = userService.findUserByIdOrThrow(senderId);
-        Owner receiver = ownerService.findOwnerByIdOrThrow(messageCreateRequest.getReceiverId());
-
-        String profilePath = lateService.saveProfileFile(attachment);
-
-        Message message = new Message();
-        message.setLate(late);
-        message.setSender(sender);
-        message.setReceiver(receiver);
-        message.setCreatedAt(LocalDateTime.now());
-        message.setAttachment(profilePath);
-
-        //내용이 있을 경우 저장
-        if (messageCreateRequest.getContent() != null) {
-            message.setContent(messageCreateRequest.getContent());
-        }
-
-        messageRepository.save(message);
-        return new MessageCreateResponse(
-                sender.getName(),
-                receiver.getName(),
-                message.getContent(),
-                message.getAttachment(),
-                message.getCreatedAt()
-        );
-    }
-
-    @Transactional
-    public PayCreateResponse createPay(Long lateId, Long senderId, PayCreateRequest payCreateRequest) throws IOException {
-        Late late = lateService.findLateByIdOrThrow(lateId);
-        User sender = userService.findUserByIdOrThrow(senderId);
-        Owner receiver = ownerService.findOwnerByIdOrThrow(payCreateRequest.getReceiverId());
-
-        Pay pay = new Pay();
-        pay.setLate(late);
-        pay.setSender(sender);
-        pay.setReceiver(receiver);
-        pay.setEnvelope(payCreateRequest.getEnvelope());
-        pay.setAmount(payCreateRequest.getAmount());
-        pay.setCreatedAt(LocalDateTime.now());
-
-        payRepository.save(pay);
-        return new PayCreateResponse(
-                sender.getName(),
-                receiver.getName(),
-                pay.getEnvelope(),
-                pay.getAmount(),
-                pay.getCreatedAt()
-        );
-    }
 
     //상주로 있는 조문공간 리스트
     public List<LateSimpleResponse> getLatelist(Long userId){
