e170560e23ff67b68932168037b578c3f00c4c6b
Answl <118355919+Answl@users.noreply.github.com>
2025-03-10 00:57:02 +0900

    [feat] 나의 기록 - 메시지 및 조의금 조회 기능

--- src/main/java/com/smwu_itple/backend/repository/PayRepository.java
+++ src/main/java/com/smwu_itple/backend/repository/PayRepository.java
@@ -3,10 +3,12 @@ package com.smwu_itple.backend.repository;
 import com.smwu_itple.backend.domain.Late;
 import com.smwu_itple.backend.domain.Message;
 import com.smwu_itple.backend.domain.Pay;
+import com.smwu_itple.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PayRepository extends JpaRepository<Pay, Long> {
     List<Pay> findByLate(Late late);
+    List<Pay> findBySender(User sender);
 }
